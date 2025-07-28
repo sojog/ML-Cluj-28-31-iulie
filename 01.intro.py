2e7dccb64d97b86f43150f2820807198d1797f91
Silviu Ojog <sil.ojog@gmail.com>
2025-07-28 11:49:09 +0300

    Intro Python

---
+++ 01.intro.py
@@ -0,0 +1,19 @@
+
+
+print('hello world')
+print("hello \n world")
+
+
+
+print("""hello
+      
+       world""")
+
+print('''hello
+      
+       world''')
+print('hello world')
+
+
+if 10 > 3:
+    print("Adevarat")
